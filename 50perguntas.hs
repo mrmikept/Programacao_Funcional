0e89aaa34edf8cbff38dd5949c7eb347a8754e2e
mrmikept <mike.pinto1507@gmail.com>
2021-10-21 17:42:20 +0100

    update

---
+++ 50perguntas.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+--Exercicio 1
+deAde::Int->Int->[Int]
+deAde x y
+    | x > y = []
+    | otherwise = x:deAde (x+1) y
+
+--Exercicio 2
+enumFromTHenTo2::Int->Int->Int->[Int]
+enumFromTHenTo2 x y z
+    |x > z = []
+    |otherwise = x:enumFromTHenTo2 (x+y-1) y z
+
+--Exercicio 3
+juntarListas::[a]->[a]->[a]
+juntarListas [] a = a
+juntarListas (a:b) l = a:juntarListas b l
+
+--Exercicio 4
+posicao::[a]->Int->a
+posicao (h:t) p
+    |p == 0 = h
+    |otherwise = posicao t (p-1)
+
+--Exercicio 5
+inverso::[a]->[a]
+inverso [] = []
+inverso (h:t)= inverso t ++ [h]
+
+--Exercicio 6
+tirar::Int->[a]->[a]
+tirar n [] = []
+tirar n (h:t)
+    |n == 1 = [h]
+    |otherwise = h:tirar (n-1) t
+
+--Exercicio 7
+cair::Int->[a]->[a]
+cair n [] = []
+cair n (h:t)
+    |n == 0 = h:t
+    |otherwise = cair (n-1) t
+
+--Exercicio 8
+myzip::[a]->[b]->[(a,b)]
+myzip [] _ = []
+myzip _ [] = []
+myzip (h1:t1) (h2:t2) = (h1,h2) : myzip t1 t2
---
+++ Ficha1.hs
@@ -0,0 +1,77 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+import Distribution.Simple.Utils (xargs)
+--Exercício 1
+perimetro::Float->Float
+perimetro r = 2*pi*r
+
+dist::(Double , Double)->(Double , Double)->Double
+dist (x1,y1) (x2,y2) = sqrt((x1-x2)^2+(y1-y2)^2)
+
+primUlt::[a]->(a , a)
+primUlt l = (head l , last l)
+
+multiplo::Int->Int->Bool
+multiplo m n = mod m n == 0
+
+truncaImpar::[a]->[a]
+truncaImpar l
+    |even (length l) = l
+    |otherwise = tail l
+
+max2::Int-> Int ->Int
+max2 x y
+    |x > y = x
+    |otherwise = y
+
+max3::Int->Int->Int->Int
+max3 x y z
+    |max2 x y > z = max2 x y
+    |otherwise = z
+
+--Exercicio 2
+
+nRaizes::Double->Double->Double->Int
+nRaizes a b c
+    | delta > 0 = 2
+    | delta == 0 = 1
+    | delta < 0 = 0
+    where delta = b^2 - 4*a*c
+
+raizes::Double->Double->Double->[Double]
+raizes a b c
+    | n == 2 = [x1, x2]
+    | n == 1 = [x1]
+    | n == 0 = []
+    where n = nRaizes a b c
+          delta = b^2 - 4*a*c
+          (x1,x2) = (((-b) + sqrt delta)/ (2*a), ((-b) - sqrt delta)/ (2*a))
+
+--Exercicio 3
+
+type Hora = (Int,Int)
+
+horaValida::Hora->Bool
+horaValida (h , m) = elem h [0..23] && elem m [0..59]
+
+horaMaior::Int->Int->Int
+horaMaior h1 h2 = if h1 > h2 then h1 else h2
+
+horaMaior2::Hora->Hora->Hora
+horaMaior2 (h1 , m1) (h2 , m2) = if horaValida(h1 , m1) && horaValida(h2 , m2)
+                                then if h1 > h2 || (h1 == h2 && m1 > m2)
+                                    then (h1 , m1)
+                                    else (h2 , m2)
+                                else error "hora invalida"
+
+horapmin::Hora->Int
+horapmin (h , m) 
+    |horaValida (h , m) = h*60+m
+
+minphora::Int->Hora
+minphora m = (div m 60 , mod m 60)
+
+difhora::Hora->Hora->Int
+difhora (h1,m1) (h2,m2) = abs (horapmin (h1,m1) - horapmin(h2 , m2))
+
+addmin::Hora->Int->Hora
+addmin (h,m1) m2 = minphora(horapmin(h,m1) + m2)
